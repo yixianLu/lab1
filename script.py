b7f807750f7da554eddbabbafedccb09f47a0b14
yixianLu <yixian@ualberta.ca>
2021-01-13 21:12:29 -0500

    init 5

--- script.py
+++ script.py
@@ -1,6 +1,6 @@
 import requests
-#print(requests.__version__)
-#r = requests.get('https://www.google.com/') 
-#print(r.text)
+print(requests.__version__)
+r = requests.get('https://www.google.com/') 
+print(r.text)
 r_p = requests.get('https://raw.githubusercontent.com/yixianLu/lab1/main/script.py')
 print(r_p.text)
